5718ffc9d2581954a600138132eb5bb0de27a344
Tierra Moultrie <tierramoultrie@gmail.com>
2020-01-29 03:43:29 +0000

    lesson 18

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,22 @@
 class PlacesController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
 
     def index
-        @places = Place.all
+      @places = Place.all
     end
-end
+  
+    def new
+      @place = Place.new
+    end
+  
+    def create
+        current_user.places.create(place_params)
+        redirect_to root_path
+      end
+  
+    private
+  
+    def place_params
+      params.require(:place).permit(:name, :description, :address)
+    end
+  end
