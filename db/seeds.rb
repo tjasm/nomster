be0bf1850342a1f4a43c5483618637875da6fb25
Tierra Moultrie <tierramoultrie@gmail.com>
2020-01-29 03:05:44 +0000

    lesson 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Place.destroy_all
+
+Place.create!([{
+  name: "Buzzmill",
+  description: "Rustic spot serves coffee, beer & cider around the clock in general-store digs with a back patio.",
+  address: "1505 Town Creek Dr, Austin, TX 78741",
+},
+{
+  name: "The Jackalope South Shore",
+  description: "Laid-back bar featuring booths & pool tables, plus a dog-friendly patio with picnic tables & games.",
+  address: "1523 Tinnin Ford Rd, Austin, TX 78741",
+},
+{
+  name: "Frazier's Long and Low",
+  description: "Dive bar",
+  address: "2538 Elmont Dr, Austin, TX 78741",
+}])
+
+p "Created #{Place.count} Place"
